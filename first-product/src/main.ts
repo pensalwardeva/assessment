d057b54f4ff7939b65e6d21282d3aa1f8b10796a
pensalwardeva <127378138+pensalwardeva@users.noreply.github.com>
2023-03-28 11:54:51 +0530

    Revert "myassessment send"

--- first-product/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { Appmodule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(Appmodule)
-  .catch(err => console.error(err));
